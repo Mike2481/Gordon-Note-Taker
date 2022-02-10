99bd457f019a90f921b48803e894c2d6c3b01c46
Mike Gordon <vprmatrix55@gmail.com>
2022-02-09 20:07:57 -0700

    add read and write

---
+++ apiRoutes/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const { store } = require ('./path to lib');
+const { notes } = require('../db/db.json');
+
+
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 // set port to 3001
 const PORT = process.env.PORT || 3001;
+const store = require('./db/store');
 
 const app = express();
 // import the db.json file
@@ -14,6 +15,8 @@ const notes = require('./db/db.json');
 //  required in order to post
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('public'));
 
 // need to return notes.html
 app.get('/notes', (req, res) => { 
@@ -26,20 +29,23 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => {  // found getNote() in public/assets/js/index.js
-    const result = notes;
-    if (result) {
-        res.json(result);
-    }
+app.get('/api/notes', (req, res) => { 
+    store.getNotes().then((notes) => {
+        return res.json(notes)
+    })
 });
 
-app.post('/api/notes', (req, res) => {  // found saveNote() in public/assets/js/index.js
+// app.post('/api/notes', (req, res) => {
+//     const notes = 
+// })
 
-}) 
+// app.post('/api/notes', (req, res) => {  // found saveNote() in public/assets/js/index.js
+
+// }) 
 //should receive a new note to save on the request body, add it to the db.json file
 // and then return the new note to the client. 
 //You'll need to find a way to give each note a unique id when it's saved 
-//(look into npm packages that could do this for you).
+//(look into npm packages that could do this for you).  uuid?
 
 
 
@@ -51,3 +57,7 @@ app.post('/api/notes', (req, res) => {  // found saveNote() in public/assets/js/
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
 // In order to delete a note, you'll need to read all notes from the db.json file, remove the note 
 // with the given id property, and then rewrite the notes to the db.json file.
+
+app.listen(PORT, () => {
+    console.log('listening on port 3001')
+})
